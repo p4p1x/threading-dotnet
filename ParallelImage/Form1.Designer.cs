2748c5fb3c555e00cd6fbc353674fd9a1719d021
wpap <mrwocior@gmail.com>
2024-05-19 22:47:00 +0200

    Dodaj pliki projektów.

---
+++ ParallelImage/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace ParallelImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Button button1;
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            button2 = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 8.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(362, 160);
+            button1.Name = "button1";
+            button1.Size = new Size(71, 57);
+            button1.TabIndex = 12;
+            button1.Text = "Load Image";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 29);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(344, 378);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(439, 29);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(176, 188);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 8;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(621, 29);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(176, 188);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 9;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(439, 223);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(176, 184);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 10;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Location = new Point(621, 223);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(176, 184);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 11;
+            pictureBox5.TabStop = false;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 8.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(362, 223);
+            button2.Name = "button2";
+            button2.Size = new Size(71, 57);
+            button2.TabIndex = 13;
+            button2.Text = "Parallel Processing";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            openFileDialog1.FileOk += openFileDialog1_FileOk;
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.Filter = "Png Images(*.png)|*.png|jpg files (*.jpg)|*.jpg|All\r\nfiles (*.*)|*.*|Bitmap Images(*.bmp)|*.bmp";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Name = "Parallel Image";
+            Text = "Parallel Image";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private Button button1;
+        private Button button2;
+        private OpenFileDialog openFileDialog1;
+    }
+}
---
+++ ParallelImage/Form1.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
+using System.Runtime.InteropServices;
+using Emgu.CV;
+using Emgu.CV.OCR;
+using Emgu.CV.Structure;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace ParallelImage
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap? img;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            openFileDialog1.ShowDialog();
+            var file = openFileDialog1.FileName;
+            if (file != null)
+            {
+                img = new Bitmap(file);
+                pictureBox1.Image = img;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            var file_name = openFileDialog1.FileName;
+
+            Parallel.Invoke(() => pictureBox2.Image = EdgeDetection(file_name),
+                            () => pictureBox3.Image = Thresholding(file_name),
+                            () => pictureBox4.Image = Negative(file_name),
+                            () => pictureBox5.Image = Mirror(file_name));
+  
+        }
+
+        public Bitmap EdgeDetectionCV(string? file_name)
+        {
+            Bitmap? bmp = new Bitmap(file_name);
+            Mat mat = new Mat();
+            mat = bmp.ToMat();
+
+            Mat mat_canny = new Mat();
+            CvInvoke.Canny(mat, mat_canny, 100, 200);
+
+            return mat_canny.ToBitmap();
+        }
+
+        public Bitmap EdgeDetection(string? file_name)
+        {
+            Bitmap? bmp = new Bitmap(file_name);
+            
+
+            return bmp;
+        }
+
+        public Bitmap Thresholding(string? file_name)
+        {
+            Bitmap? bmp = new Bitmap(file_name);
+            Mat mat = new Mat();
+            mat = bmp.ToMat();
+
+            
+            Mat thresholding_mat = new Mat();
+            CvInvoke.Threshold(mat, thresholding_mat, 127, 255, Emgu.CV.CvEnum.ThresholdType.Binary);
+
+            return thresholding_mat.ToBitmap();
+        }
+
+        public Bitmap Negative(string? file_name)
+        {
+            Bitmap? bmp = new Bitmap(file_name);
+
+            int w = bmp.Width;
+            int h = bmp.Height;
+            for (int x = 0; x < w; ++x)
+            {
+                for (int y = 0; y < h; ++y)
+                {
+                    Color pixelColor = bmp.GetPixel(x, y);
+                    Color newColor = Color.FromArgb(0xff - pixelColor.R
+                    , 0xff - pixelColor.G, 0xff - pixelColor.B);
+                     bmp.SetPixel(x, y, newColor);
+                }
+            }
+            return bmp;
+             
+        }
+        public Bitmap Mirror(string? file_name)
+        {
+            Bitmap? bmp = new Bitmap(file_name);
+            bmp.RotateFlip(RotateFlipType.RotateNoneFlipX); 
+            return bmp;
+        }
+
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
